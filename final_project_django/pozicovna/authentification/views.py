444e6eb9b25521924e5a96b7929fee267bc1be76
MikaKlimek <klimek.mika@gmail.com>
2024-04-18 17:50:19 +0200

    :sparkles: second last second changes

--- final_project_django/pozicovna/authentification/views.py
+++ final_project_django/pozicovna/authentification/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from core.models import Car
 
 # Create your views here.
 def signup(request):
@@ -32,7 +33,18 @@ def logout(request):
 
 @login_required
 def view_profile(request):
-    context = {'user' : request.user}
-    template = loader.get_template("profile.html")
 
-    return HttpResponse(template.render(context=context, request=request))
+    try:
+        cars = get_list_or_404(Car, current_user = request.user.profile)
+
+    except:
+        context = {'user' : request.user}
+        template = loader.get_template("profile.html")
+        return HttpResponse(template.render(context=context, request=request))
+    
+    else:
+        cars = get_list_or_404(Car, current_user = request.user.profile)
+        context = {'user' : request.user,
+               'cars' : cars}
+        template = loader.get_template("profile_with_cars.html")
+        return HttpResponse(template.render(context=context, request=request))
